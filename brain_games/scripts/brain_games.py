68baab5774bbd6d1ead5bbf7f556ab5e2dd9f646
Vadim.S <wadimator@gmail.com>
2022-10-30 22:58:48 -0500

    first task

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.9
+
+def main():
+
+	print("poetry run python -m brain_games.scripts.brain_games\nWelcome to the Brain Games!")
+    
+if __name__ == '__main__':
+    main()
+
